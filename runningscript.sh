ce696ae1995067b53b30b0e46a3cc7df3922aeb9
WillMatt4 <49714371+WillMatt4@users.noreply.github.com>
2022-06-02 15:54:53 +0200

    Add files via upload

---
+++ runningscript.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+##SBATCH --partition=shared-cpu
+##SBATCH --partition=public-cpu
+#SBATCH --partition=debug-cpu
+##SBATCH --partition=public-longrun-cpu
+
+##SBATCH --exclude=cpu[116-119]
+
+#SBATCH --ntasks=1
+##SBATCH --time=24:00:00
+
+#SBATCH --mail-type=ALL
+
+#SBATCH -J test0##rdn0_1000
+#SBATCH --output=slurm-%x-%J.out
+
+
+echo "Starting at `date`"
+echo "Running on hosts: $SLURM_NODELIST"
+echo "Running on $SLURM_NNODES nodes."
+echo "Running on $SLURM_NPROCS processors."
+echo "Current working directory is `pwd`"
+echo ""
+echo "***** LAUNCHING *****"
+echo `date '+%F %H:%M:%S'`
+echo ""
+
+# load Anaconda and OpenMPI
+module load Anaconda3
+module load foss
+module load class
+
+echo "Loaded Anaconda3, Class and foss"
+echo ""
+
+# # #OpenMP settings:
+# export OMP_NUM_THREADS=2
+# export OMP_PLACES=threads
+# export OMP_PROC_BIND=spread
+
+
+
+#launch simulations
+srun python3 SMAL-FRY.py##RDN0_parallel_script.py
+
+echo ""
+echo "***** DONE *****"
+echo `date '+%F %H:%M:%S'`
+echo ""
