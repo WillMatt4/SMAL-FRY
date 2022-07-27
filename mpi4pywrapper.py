6e523d2a75b4b6ef2574dcad6ee652cc106202d8
WillMatt4 <49714371+WillMatt4@users.noreply.github.com>
2022-07-27 12:09:33 +0200

    Update mpi4pywrapper.py

--- mpi4pywrapper.py
+++ mpi4pywrapper.py
@@ -3,10 +3,11 @@
 from __future__ import print_function
 import os
 import sys
+sys.path.append('/home/users/m/matthews/.local/lib/python3.10/site-packages')
 
 verbose = True#False
 if 'SLURM_SUBMIT_DIR' in os.environ.keys():
-    #try:
+    try:
         from mpi4py import MPI
         print("success?'\n'")
         rank = MPI.COMM_WORLD.Get_rank()
@@ -15,8 +16,8 @@ if 'SLURM_SUBMIT_DIR' in os.environ.keys():
         finalize = MPI.Finalize
         if verbose:
             print('pbs.py : setup OK, rank %s in %s' % (rank, size))
-    #except:
-    #    if verbose: sys.stderr.write('pbs.py: unable to setup mpi4py\n')
+    except:
+        if verbose: sys.stderr.write('pbs.py: unable to setup mpi4py\n')
 elif 'NERSC_HOST' not in os.environ.keys():
     try:
         from mpi4py import MPI
