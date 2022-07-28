13ddcb01459c0332888da96773c0237994beafb3
WillMatt4 <49714371+WillMatt4@users.noreply.github.com>
2022-07-28 13:13:41 +0200

    Update runningscript.sh

--- runningscript.sh
+++ runningscript.sh
@@ -36,10 +36,11 @@ echo ""
 #module load GCCcore/8.3.0
 #module load glibc/2.30
 module load Anaconda3
-#module load GCC/11.3.0
-#module load OpenMPI/4.1.4
+module load GCC/11.3.0
+module load OpenMPI/4.1.4
 #module load foss
-#conda activate myenv
+conda activate myenv
+module load mpi4py/3.0.3-timed-pingpong
 
 #echo "Loaded Anaconda3 and foss"
 echo ""
