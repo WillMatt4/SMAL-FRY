04f73cc035a289038a5e70f3ddea007ea4884e8d
WillMatt4 <49714371+WillMatt4@users.noreply.github.com>
2022-07-29 14:45:39 +0200

    Update runningscriptCLASS.py

--- runningscriptCLASS.py
+++ runningscriptCLASS.py
@@ -1,10 +1,10 @@
 #!/bin/sh
 ##up to 12h:
-##SBATCH --partition=shared-cpu
+#SBATCH --partition=shared-cpu
 ##12h - 4days:
 ##SBATCH --partition=public-cpu
 ##up to 15min:
-#SBATCH --partition=debug-cpu
+##SBATCH --partition=debug-cpu
 ##max. of 2 cores for 14 days:
 ##SBATCH --partition=public-longrun-cpu
 
@@ -12,7 +12,7 @@
 
 #SBATCH --ntasks=6
 ##SBATCH --cpus-per-task=1  
-##SBATCH --time=24:00:00
+#SBATCH --time=00:20:00
 
 ##SBATCH --mail-type=ALL
 
