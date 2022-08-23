b750550c4bd93055335f01d820002c653fa5a642
WillMatt4 <49714371+WillMatt4@users.noreply.github.com>
2022-08-23 11:36:24 +0200

    Update SMAL-FRY.py

--- SMAL-FRY.py
+++ SMAL-FRY.py
@@ -549,15 +549,15 @@ if RUN_SPECTRA==1:
   #FIDUCIAL:
   ##########
   itr = mpi.rank
-  if itr==0:
-      Spectra(0,params,'')
+  #if itr==0:
+  #    Spectra(0,params,'')
   
   #mpi.barrier #wait for fiducial run to complete (background to be generated)
     
-  if itr!=0:  
-  #if itr>=0:  
-      key = Keys[itr-1]
-      #key = Keys[itr]
+  #if itr!=0:  
+  if itr>=0:  
+      #key = Keys[itr-1]
+      key = Keys[itr]
   #PLUS STEP:
   ###########
       Spectra(1,params,key)
