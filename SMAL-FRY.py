af12044f7424e267c5fb47b127ed9a9c8e70e94a
WillMatt4 <49714371+WillMatt4@users.noreply.github.com>
2022-08-23 10:10:26 +0200

    Update SMAL-FRY.py

--- SMAL-FRY.py
+++ SMAL-FRY.py
@@ -154,9 +154,9 @@ biasno = [0.79,0.5887]
 biasno2 = [0.68,0.8130]
 surv = [i for i in range(len(N)) if N[i]>0]
 
-params['b1'] = biasno[surv[0]]                         #1st bias parameter
-params['b2'] = biasno2[surv[0]]                        #2nd bias parameter
-print('\n\nb1: '+str(params['b1'])+' b2: '+str(params['b2'])+'\n\n')
+#params['b1'] = biasno[surv[0]]                         #1st bias parameter
+#params['b2'] = biasno2[surv[0]]                        #2nd bias parameter
+#print('\n\nb1: '+str(params['b1'])+' b2: '+str(params['b2'])+'\n\n')
 
 
 #Noise and Fisher limits:
@@ -165,7 +165,7 @@ print('\n\nb1: '+str(params['b1'])+' b2: '+str(params['b2'])+'\n\n')
 # NB! ONLY MAXIMUM OF ONE (Marg OR Fix) MAY BE NON-ZERO:
 # The Fix and Marg parameters have NOT been tested in this version of the code and should be checked if used [TBC]. 
 Fix = 0                                          # Number of parameters to fix (remove before inversion) (starting at the parameter with the highest index).
-Marg = 2                                         # Number of parameters to marginalise (remove after inversion) over (starting at the parameter with the highest index)
+Marg = 0#2                                         # Number of parameters to marginalise (remove after inversion) over (starting at the parameter with the highest index)
 # Currently, if left as default (0), the code will produce the conditional and marginalized errors (but no additional parameters are marginalized over).
 Cond = 1                                         # Should conditional errors be found? YES: 1, NO: 0.
 
@@ -368,8 +368,8 @@ def RUNCLASS(signV,params,key):
     for i in range(len(bins)):
          
          # Linear bisa b(z) and magnification bias s(z) parameters for input to CLASS.
-         BIASNO = params['b1']
-         BIASNO2 = params['b2']
+         BIASNO = biasno[surv[0]]# params['b1']
+         BIASNO2 = biasno2[surv[0]]#params['b2']
          #print(BIASNO,BIASNO2)
          s0 = -0.106875
          s1 = 1.35999
