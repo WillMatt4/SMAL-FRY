3ea4ca509cada8dc1d7a64a3bc7017f01af24fd6
WillMatt4 <49714371+WillMatt4@users.noreply.github.com>
2022-08-23 11:38:05 +0200

    Update SMAL-FRY.py

--- SMAL-FRY.py
+++ SMAL-FRY.py
@@ -108,9 +108,9 @@ start=time.time()
 ################################
 #COSMOLOGY AND INPUTS FOR CLASS:
 ################################
-RUN_SPECTRA = 1                                   # Should CLASS be used to generate the necessary spectra? YES: 1, NO: 0.
-EXTRACT_ZETA = 1                                  # Should the redshift-weighted number count power spectrum? YES: 1, NO: 0.
-RUN_FISHER = 0                                    # Should the Fisher analysis be run (DOESN'T REQUIRE MULTIPLE CORES)
+RUN_SPECTRA = 0                                   # Should CLASS be used to generate the necessary spectra? YES: 1, NO: 0.
+EXTRACT_ZETA = 0                                  # Should the redshift-weighted number count power spectrum? YES: 1, NO: 0.
+RUN_FISHER = 1                                    # Should the Fisher analysis be run (DOESN'T REQUIRE MULTIPLE CORES)
 CLASSPATH = '/home/users/m/matthews/scratch/Zeta/SMAL-FRY/class_public-3.0.1_mod/' #
 OUTPATH = '/home/users/m/matthews/scratch/Zeta/outputs/' #
 NO = '05'                                         # This Number relates to the filename and numbering scheme of the input CLASS spectra.
@@ -549,15 +549,15 @@ if RUN_SPECTRA==1:
   #FIDUCIAL:
   ##########
   itr = mpi.rank
-  #if itr==0:
-  #    Spectra(0,params,'')
+  if itr==0:
+      Spectra(0,params,'')
   
   #mpi.barrier #wait for fiducial run to complete (background to be generated)
     
-  #if itr!=0:  
-  if itr>=0:  
-      #key = Keys[itr-1]
-      key = Keys[itr]
+  if itr!=0:  
+  #if itr>=0:  
+      key = Keys[itr-1]
+      #key = Keys[itr]
   #PLUS STEP:
   ###########
       Spectra(1,params,key)
