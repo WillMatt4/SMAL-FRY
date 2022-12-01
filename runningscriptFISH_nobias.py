8a42d8248c662cf1f850d29e6db4fc8b627e8e3e
WillMatt4 <49714371+WillMatt4@users.noreply.github.com>
2022-12-01 13:40:40 +0100

    Create runningscriptFISH_nobias.py

---
+++ runningscriptFISH_nobias.py
@@ -0,0 +1,63 @@
+#!/bin/sh
+##up to 12h:
+#SBATCH --partition=shared-cpu
+##12h - 4days:
+##SBATCH --partition=public-cpu
+##up to 15min:
+##SBATCH --partition=debug-cpu
+##max. of 2 cores for 14 days:
+##SBATCH --partition=public-longrun-cpu
+
+##SBATCH --exclude=cpu[116-119]
+
+#SBATCH --ntasks=1
+##SBATCH --cpus-per-task=1  
+#SBATCH --time=10:00:00
+
+#SBATCH --mail-type=ALL
+
+#SBATCH -J deltazeta_fisher##test0##rdn0_1000
+#SBATCH --output=slurm-%x-%J.out
+
+
+echo "Starting at `date`"
+echo "Running on hosts: $SLURM_NODELIST"
+echo "Running on $SLURM_NNODES nodes."
+echo "Running on $SLURM_NPROCS processors."
+echo "Current working directory is `pwd`"
+echo ""
+echo "***** LAUNCHING *****"
+echo `date '+%F %H:%M:%S'`
+echo ""
+
+# load Anaconda and OpenMPI
+#module load GCCcore/6.4.0
+#module load glibc/2.17
+#module load GCCcore/8.3.0
+#module load glibc/2.30
+#module load Anaconda3
+##module load GCC/10.2.0
+##module load OpenMPI/4.0.5
+#module load foss
+#conda activate myenv
+##module load mpi4py/3.0.3-timed-pingpong
+#module load numpy/1.18.5-Python-3.6.6
+#module load scipy/1.4.1-Python-3.7.4
+
+#echo "Loaded Anaconda3 and foss"
+echo ""
+
+# # #OpenMP settings:
+# export OMP_NUM_THREADS=2
+# export OMP_PLACES=threads
+# export OMP_PROC_BIND=spread
+
+
+
+#launch simulations
+srun python3 SMAL-FRY_nobias.py
+
+echo ""
+echo "***** DONE *****"
+echo `date '+%F %H:%M:%S'`
+echo ""
